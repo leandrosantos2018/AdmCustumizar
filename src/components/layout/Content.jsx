349f2db4fb15ee56073b37d6e4090cab70169a36
Leandro Santos <leleuramos@gmail.com>
2021-11-07 09:20:11 -0300

    concluido modulo router

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -5,6 +5,7 @@ import { Route, Switch } from 'react-router-dom';
 import About from '../../views/examples/About'
 import CadastroUsers from '../../views/Operacional/Cadastro';
 import Home from '../../views/examples/Home'
+import NotFound from '../../views/examples/NotFound';
 import Param from '../../views/examples/Param'
 import React from 'react';
 
@@ -30,6 +31,9 @@ const Content = props => (
 
       <Route exact path="/">
         <Home />
+      </Route>
+      <Route exact path="*">
+        <NotFound />
       </Route>
     </Switch>
   </main>
